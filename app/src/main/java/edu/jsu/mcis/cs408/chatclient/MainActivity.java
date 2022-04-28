2903f7436e91cd10ac5a19932d8294cb7cbcf1e6
TJordan <Tjordan7@stu.jsu.edu>
2022-04-28 00:03:06 -0500

    begin debugging WebServiceViewModel line 144

---
+++ app/src/main/java/edu/jsu/mcis/cs408/chatclient/MainActivity.java
@@ -0,0 +1,62 @@
+package edu.jsu.mcis.cs408.chatclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+
+import org.json.JSONObject;
+
+import edu.jsu.mcis.cs408.chatclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private WebServiceViewModel model;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        model = new ViewModelProvider(this).get(WebServiceViewModel.class);
+
+        final Observer<String> outputObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s != null) {
+                    setOutputText(s);
+                }
+            }
+        };
+
+        model.getOutput().observe(this, outputObserver);
+
+        binding.postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = binding.input.getText().toString();
+                if (input != "") { //TODO always be posting
+                    model.sendMessage(input);
+                } else {
+                    model.generateOutput();
+                }
+            }
+        });
+
+        binding.clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                model.clearChat();
+            }
+        });
+    }
+
+    private void setOutputText(String output) {
+        binding.Output.setText(output);
+    }
+}
---
+++ app/src/main/java/edu/jsu/mcis/cs408/chatclient/WebServiceViewModel.java
@@ -0,0 +1,185 @@
+package edu.jsu.mcis.cs408.chatclient;
+
+import static java.net.HttpURLConnection.HTTP_CREATED;
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class WebServiceViewModel extends ViewModel {
+    private static final String TAG = "WebServiceDemoViewModel";
+
+    private static final String CLIENT_URL = "http://ec2-3-143-211-101.us-east-2.compute.amazonaws.com/CS408_SimpleChat/Chat";
+    private final ExecutorService requestThreadExecutor;
+    private final Runnable httpGetRequestThread, httpPostRequestThread, httpDeleteRequestThread;
+    private Future<?> pending;
+
+    private MutableLiveData<String> output;
+
+    private String message;
+
+    public WebServiceViewModel() {
+        requestThreadExecutor = Executors.newSingleThreadExecutor();
+
+        httpGetRequestThread = new Runnable() {
+            @Override
+            public void run() {
+                if (pending != null) {
+                    pending.cancel(true);
+                }
+
+                try {
+                    pending = requestThreadExecutor.submit(new HTTPRequestTask("GET", CLIENT_URL));
+                } catch (Exception e) {
+                    Log.e(TAG, " Exception: ", e);
+                }
+            }
+        };
+
+        httpPostRequestThread = new Runnable() {
+            @Override
+            public void run() {
+                if (pending != null) { pending.cancel(true); }
+
+                try {
+                    pending = requestThreadExecutor.submit(new HTTPRequestTask("POST", CLIENT_URL));
+                }
+                catch (Exception e) { Log.e(TAG, " Exception: ", e); }
+            }
+        };
+
+        httpDeleteRequestThread = new Runnable() {
+            @Override
+            public void run() {
+                if (pending != null) { pending.cancel(true); }
+
+                try {
+                    pending = requestThreadExecutor.submit(new HTTPRequestTask("DELETE", CLIENT_URL));
+                }
+                catch (Exception e) { Log.e(TAG, " Exception: ", e); }
+            }
+        };
+    }
+
+    public void generateOutput() {
+        httpGetRequestThread.run();
+    }
+
+    private void setOutput(String s) {
+        this.getOutput().postValue(s);
+    }
+
+    public void sendMessage(String message) {
+        this.message = message;
+        httpPostRequestThread.run();
+    }
+
+    public void clearChat() {
+        httpDeleteRequestThread.run();
+    }
+
+    public MutableLiveData<String> getOutput() {
+        if (output == null) {
+            output = new MutableLiveData<>();
+        }
+        return output;
+    }
+
+
+
+    final class HTTPRequestTask implements Runnable {
+        private static final String TAG = "HTTPRequestTask";
+        private final String method, urlString;
+
+        HTTPRequestTask(String method, String urlString) {
+            this.method = method;
+            this.urlString = urlString;
+        }
+
+        @Override
+        public void run() {
+            setOutput(doRequest());
+        }
+
+
+        private String doRequest() {
+            StringBuilder r = new StringBuilder();
+            String line;
+
+            HttpURLConnection conn = null;
+            String input = null;
+
+            try {
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+
+                URL url = new URL(CLIENT_URL);
+
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod(method);
+                conn.setDoInput(true);
+
+                if (method.equals("POST")) {
+                    JSONObject outputJSON = new JSONObject();
+                    outputJSON.put("message", message);
+
+                    conn.setDoOutput(true);
+                    OutputStream out = conn.getOutputStream();
+                    out.write(outputJSON.toString().getBytes());
+                    out.flush();
+                    out.close();
+                }
+
+                conn.connect();
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+
+                int code = conn.getResponseCode();
+
+                if (code == HTTP_OK || code == HTTP_CREATED) {  //TODO do I need a third case for delete?
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    do {
+                        line = br.readLine();
+                        if (line != null) {
+                            r.append(line);
+                        }
+                    } while(line != null);
+                }
+
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+
+                JSONObject jo = new JSONObject(r.toString());
+                input =  (String)jo.get("messages");
+
+            } catch (Exception e) {
+                Log.e(TAG, " Exception: ", e);
+            } finally {
+                if (conn != null) { conn.disconnect(); }
+            }
+
+            return input;
+        }
+    }
+}
+
